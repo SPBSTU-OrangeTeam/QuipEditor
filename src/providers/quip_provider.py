40e1aa7eae352524e356e23c81dddd1f4e031ffb
Andrey Cherevichnik <ioctav@icloud.com>
2020-10-25 22:05:31 +0300

    Move quip.py in correct folder

--- src/providers/quip_provider.py
+++ src/providers/quip_provider.py
@@ -4,7 +4,7 @@ from ..deps.quip import QuipClient
 
 class QuipProvider:
     def __init__(self):
-        self.quip_client = quip.QuipClient(
+        self.quip_client = QuipClient(
             access_token=sublime.load_settings("SublimeQuip.sublime-settings").get("quip_token", "NOT_FOUND"),
             base_url="https://platform.quip.com")
 
