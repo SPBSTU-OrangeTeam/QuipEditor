dad59da0fa2b7d6f5bd656e60fcb4950ab75afd9
Kirillova Jenya <mandarin1029@gmail.com>
2020-12-09 15:00:55 +0300

    Fix merge 9ab1175

--- SublimeQuip.py
+++ SublimeQuip.py
@@ -4,7 +4,6 @@ import json
 import os
 from .src.providers import quip_provider
 from .CurrentManager import CurrentManager
-<<<<<<< HEAD
 from .TabsManager import TabsManager
 from .TabsManager import FILE_TREE_TAB_ID
 
@@ -19,10 +18,7 @@ COMMAND_GET_RANDOM_DOCUMENT = "insert_random_document_html"
 
 KEY_THREAD_ID = "thread_id"
 KEY_FILE_TREE_PHANTOM_SET = "file_tree_phantom_set"
-=======
-import os
 
->>>>>>> develop
 
 current = CurrentManager()
 tabs_manager = TabsManager()
@@ -46,6 +42,7 @@ class OpenRecentDocumentCommand(sublime_plugin.WindowCommand):
 			view = self.window.new_file()
 			view.run_command(COMMAND_GET_RANDOM_DOCUMENT)
 
+
 class ShowFileTreeCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		if not tabs_manager.contains_tab(FILE_TREE_TAB_ID):
@@ -56,6 +53,7 @@ class ShowFileTreeCommand(sublime_plugin.WindowCommand):
 			self.window.focus_view(view)
 		view.run_command(COMMAND_PRINT_QUIP_FILE_TREE)
 
+
 class InsertRandomDocumentHtmlCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		quipprovider = quip_provider.QuipProvider()
@@ -64,7 +62,7 @@ class InsertRandomDocumentHtmlCommand(sublime_plugin.TextCommand):
 		current.add(view=self.view, thread=id)
 		self.view.insert(edit, 0, quipprovider.get_document_content(id))
 
-<<<<<<< HEAD
+
 class InsertSelectedDocumentCommand(sublime_plugin.TextCommand):
 	def run(self, edit, **args):
 		doc_id = args[KEY_THREAD_ID]
@@ -75,7 +73,45 @@ class InsertSelectedDocumentCommand(sublime_plugin.TextCommand):
 
 class PrintQuipFileTree(sublime_plugin.TextCommand):
 	def __print_tree(self, tree_node, prefix, postfix):
-=======
+		if tree_node is None:
+			return ""
+		thread_name = tree_node.name
+		if tree_node.thread_type == 'document':
+			thread_name = '<a href=\'{0}\'>{1}</a>'.format(tree_node.thread_id, tree_node.name)
+		str_result = '{0} Name: {1} | Type: {2}{3}'.format(
+			prefix,
+			thread_name,
+			tree_node.thread_type,
+			postfix
+		)
+		if tree_node.children is None:
+			return str_result
+		for child in tree_node.children:
+			str_result += '<ul>'
+			str_result += self.__print_tree(child, "<li>", "</li>")
+			str_result += '</ul>'
+		return str_result
+
+	def run(self, edit):
+		self.view.erase_phantoms(KEY_FILE_TREE_PHANTOM_SET)
+		self.view.set_read_only(True)
+		self.view.set_name("Folders")
+		quipprovider = quip_provider.QuipProvider()
+		file_tree = quipprovider.get_thread_tree()
+		string_tree = self.__print_tree(file_tree, "", "")
+		phantom = sublime.Phantom(
+			region = self.view.visible_region(),
+			content = string_tree,
+			layout = sublime.LAYOUT_INLINE,
+			on_navigate = self.__open_doc
+		)
+		sublime.PhantomSet(self.view, KEY_FILE_TREE_PHANTOM_SET)\
+			.update([phantom, phantom]) #TODO разобраться почему только так работает
+
+	def __open_doc(self, doc_id):
+		self.view.window().run_command(COMMAND_OPEN_DOCUMENT, {KEY_THREAD_ID: doc_id})
+
+
 class OpenChatCommand(sublime_plugin.WindowCommand):
 
 	quip = quip_provider.QuipProvider()
@@ -133,7 +169,6 @@ class InsertChatMessagesCommand(sublime_plugin.TextCommand):
 		current.chat.set_scratch(True)
 
 
-
 class SendChatMessageCommand(sublime_plugin.TextCommand):
 	
 	def run(self, edit):
@@ -145,48 +180,6 @@ class SendChatMessageCommand(sublime_plugin.TextCommand):
 		quip = quip_provider.QuipProvider()
 		message = quip.send_message(current.chat_id, text).to_json()
 		current.chat.run_command("insert_chat_messages", {"messages": [message]})
-
-
-class Printquipfiletree(sublime_plugin.TextCommand):
-	def __print_tree(self, tree_node, prefix):
->>>>>>> develop
-		if tree_node is None:
-			return ""
-		thread_name = tree_node.name
-		if tree_node.thread_type == 'document':
-			thread_name = '<a href=\'{0}\'>{1}</a>'.format(tree_node.thread_id, tree_node.name)
-		str_result = '{0} Name: {1} | Type: {2}{3}'.format(
-			prefix,
-			thread_name,
-			tree_node.thread_type,
-			postfix
-		)
-		if tree_node.children is None:
-			return str_result
-		for child in tree_node.children:
-			str_result += '<ul>'
-			str_result += self.__print_tree(child, "<li>", "</li>")
-			str_result += '</ul>'
-		return str_result
-
-	def run(self, edit):
-		self.view.erase_phantoms(KEY_FILE_TREE_PHANTOM_SET)
-		self.view.set_read_only(True)
-		self.view.set_name("Folders")
-		quipprovider = quip_provider.QuipProvider()
-		file_tree = quipprovider.get_thread_tree()
-		string_tree = self.__print_tree(file_tree, "", "")
-		phantom = sublime.Phantom(
-			region = self.view.visible_region(),
-			content = string_tree,
-			layout = sublime.LAYOUT_INLINE,
-			on_navigate = self.__open_doc
-		)
-		sublime.PhantomSet(self.view, KEY_FILE_TREE_PHANTOM_SET)\
-			.update([phantom, phantom]) #TODO разобраться почему только так работает
-
-	def __open_doc(self, doc_id):
-		self.view.window().run_command(COMMAND_OPEN_DOCUMENT, {KEY_THREAD_ID: doc_id})
 
 
 class UploadChangesOnSave(sublime_plugin.EventListener):
