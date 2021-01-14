5152d52a9eccbe2fd1026a790c4ccb2a8f6aab74
Andrey Cherevichnik <ioctav@icloud.com>
2021-01-15 00:46:30 +0300

    Fix upload files

--- QuipEditor.py
+++ QuipEditor.py
@@ -328,11 +328,6 @@ class UploadChangesOnSave(sublime_plugin.EventListener):
 		thread = manager.get_thread(view)
 		if thread is None or thread == TREE_VIEW_TAB_ID:
 			return
-		view.window().run_command("set_layout", {
-			"cols": [0, 0.6, 1.0],
-			"rows": [0.0, 1.0],
-			"cells": [[0, 0, 1, 1], [1, 0, 2, 1]]
-		})
 		if manager.update_debounced(thread):
 			view.run_command(COMMAND_INSERT_SELECTED_DOCUMENT, {"thread_id": thread, "chat": False})
 			manager.event_propagation = False
